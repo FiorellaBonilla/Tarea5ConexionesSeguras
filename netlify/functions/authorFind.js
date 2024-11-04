9d65c2445b81bb1bb141be72d005233e1ae6fd58
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2024-11-04 16:16:21 -0600

    first commit

---
+++ netlify/functions/authorFind.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const clientPromise = require('./mongoDB');
+const headers = require('./headersCORS');
+
+exports.handler = async (event, context) => {
+
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+
+  try {
+    const client = await clientPromise;
+    const id = parseInt(event.path.split("/").reverse()[0]);
+
+ 
+    const author =
+     await client.db("bookstore").collection("authors").find({_id: id}).toArray();
+
+    return { statusCode: 200, headers, body: JSON.stringify(author) };
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};
---
+++ src/README.md
@@ -0,0 +1,5 @@
+Estudiante:Fiorella Bonilla González
+Link de la tarea publicada en netlify:https://tarea3mongodb.netlify.app/
+
+
+Variable usada en netlify:mongodb+srv://fiorella:1234@cluster0.06k43.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
